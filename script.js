eac2973f00828ec4d3d320cd6d42b719790fefa1
Emily Virata <virataemily@gmail.com>
2025-12-15 18:14:30 -0700

    Added reset capabilities

--- script.js
+++ script.js
@@ -13,7 +13,13 @@ function gameboard() {
     //---Methods we'll use and not return (Private)---
     
     //---Methods we'll return (Public)---
-
+    function reset() {
+        for(i=0; i<=2; i++) {
+            for(j=0; j<=2; j++){
+                board[i][j] = 0;
+            }
+        }
+    }
     //return board
     function getBoard() {
         return board;
@@ -66,7 +72,7 @@ function gameboard() {
             }
         }
         //diagonal bottom left -> top right
-        if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[0][0] != 0) {
+        if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != 0) {
             if(board[2][0] == 1) {
                 return 1;
             }
@@ -81,13 +87,14 @@ function gameboard() {
             
     }
     return {
-        turnIndex, getBoard, mark, displayBoard, checkWins
+        turnIndex, getBoard, mark, displayBoard, checkWins, reset
     };
 }
 function GameController() {
     const board = gameboard();
 
     let turnIndex = 0;
+    //---Private Method---
     function chooseToken(turnIndex) {
         if(turnIndex % 2 == 0) {
             return 1;
@@ -96,41 +103,83 @@ function GameController() {
             return 2;
         }
     }
+    //---Public Method---
     function playTurn(row, column) {
         board.mark(row, column, chooseToken(turnIndex));
         console.log(board.checkWins());
         turnIndex +=1;
+        screen.render();
+    }
+    function resetGame() {
+        board.reset();
+        turnIndex = 0;
     }
     return {
-        board, playTurn
+        board, playTurn, resetGame
     };
 }
 function ScreenController() {
+    resultText = document.querySelector("#resultText");
+    gameButtons.forEach(button => {
+        button.addEventListener("click", handleClick);
+    })
+    
     function render() {
-        const board = gameboard();
+        const board = game.board.getBoard();
         gameButtons.forEach(button => {
-            button.addEventListener("click", () => {
-                const row = Number(button.dataset.row);
-                const col = Number(button.dataset.col);
+            const row = Number(button.dataset.row);
+            const col = Number(button.dataset.col);
                 
-                const squareValue = board[row][col];
-                if(squareValue == 1) {
-                    button.textContent ="X"
-                }
-            })
+            const squareValue = board[row][col];
+            if(squareValue == 1) {
+                button.textContent ="x";
+            }
+            else if (squareValue == 2) {
+                button.textContent = "o";
+            }
+            else if(squareValue == 0) {
+                button.textContent="";
+            }
         });
-    }  
+        if(game.board.checkWins() == 1) {
+            resultText.textContent = "Player X won!";
+        }
+        else if(game.board.checkWins() == 2) {
+            resultText.textContent = "Player O won!";
+        }
+        else if(game.board.checkWins() == "tie") {
+            resultText.textContent = "It's a tie!";
+        }
+    }
+
+    function handleClick(e) {
+        const row = Number(e.target.dataset.row);
+        const col = Number(e.target.dataset.col);
+
+        game.playTurn(row, col);
+        render();
+    }
+    const resetButton = document.querySelector("#resetButton");
+    resetButton.addEventListener("click", () => {
+        game.resetGame();
+        render();
+    })
+    return {
+        render, 
+    };
+      
 }
 const game = GameController();
-game.playTurn(0,0);
-game.playTurn(0,2);
-game.playTurn(0,1)
-game.playTurn(1,0)
-game.playTurn(1,2)
-game.playTurn(1,1)
-game.playTurn(2,0)
-game.playTurn(2,2)
-game.playTurn(2,1)
+const screen = ScreenController();
+// game.playTurn(0,0);
+// game.playTurn(0,2);
+// game.playTurn(0,1)
+// game.playTurn(1,0)
+// game.playTurn(1,2)
+// game.playTurn(1,1)
+// game.playTurn(2,0)
+// game.playTurn(2,2)
+// game.playTurn(2,1)
 
 
 
